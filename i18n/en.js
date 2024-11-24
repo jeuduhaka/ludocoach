cc9534c4721c046e7c7529f12a4cf0e8f1907660
Florent Roques <florentroques@users.noreply.github.com>
2024-11-24 23:05:26 +0100

    add english and chinese + fallback to english

---
+++ i18n/en.js
@@ -0,0 +1,123 @@
+translations['en'] = {
+  loading: 'loading',
+
+  //Jeu
+  hakaGame: "The Haka's Game",
+  guidedByLudocoach: 'guided by the ludocoach Tehotu',
+  play3Moves: 'Play with 3 moves',
+  play1Move: 'Play with 1 move',
+  accessTraining: 'Access to trainings',
+  allRightsReserved: 'All rights reserved',
+  powerIsInYou: '"The Power is in you!"',
+
+  chooseCards:
+    'Choose a card of each color according to your emotions and let yourself be guided by tapping each corresponding card in order',
+  oneMoveDescription:
+    'For this version, choose the card which will help you directly reach a state of better-Being',
+  having: 'HAVING',
+  doing: 'DOING',
+  being: 'BEING',
+  play: 'Play',
+  youHaveChosen: 'You have chosen',
+  placeYourself:
+    '. Find a place where you feel good and have enough space. Pay attention to your breath and follow me.',
+  next: 'Next',
+  iChooseThisCard: 'I choose this card',
+  chooseAnotherCard: 'Choose another card',
+  manaActivated:
+    'Congratulations, you have activated your Mana! Say to yourself: "The Power is in me!"',
+  thankYou: 'Thank you',
+
+  //Menus
+  backToGame: 'Back to Game',
+  gameGoalTitle: "Game's goal",
+  adviceMenuTitle: 'Advice',
+  sendGiftCardLabel: 'Send a gift card',
+  sendGreetingCardLabel: 'Send a greeting card',
+  shareAppLabel: 'Share the app',
+  contactUs: 'Contact us',
+  authorsTitle: 'Authors',
+  gamesListTitle: "Marc Kucharz's Games",
+  acknowledgementsMenu: 'Acknowledgements',
+  acknowledgementsText: 'Thanks to',
+  onFacebook: 'On Facebook',
+  developedBy: 'Developed by',
+
+  //Cartes
+  abandonment: 'Abandonment',
+  anger: 'Anger',
+  disgust: 'Disgust',
+  doubt: 'Doubt',
+  threat: 'Threat',
+  fear: 'Fear',
+  sadness: 'Sadness',
+  violence: 'Violence',
+  'joker-red': 'Joker',
+  action: 'Action',
+  courage: 'Courage',
+  movement: 'Movement',
+  patience: 'Patience',
+  preparation: 'Preparation',
+  prevention: 'Prevention',
+  protection: 'Protection',
+  unity: 'Unity',
+  'joker-orange': 'Joker',
+  love: 'Love',
+  calm: 'Calm',
+  confidence: 'Confidence',
+  energy: 'Energy',
+  'self-esteem': 'Self-esteem',
+  strength: 'Strength',
+  joy: 'Joy',
+  peace: 'Peace',
+  'joker-green': 'Joker',
+
+  //game's goal section
+  transformYourEnergy:
+    'Transform your negative energy in positive energy thanks to ancestral maori dances!',
+  limitingEmotion: ' a limiting emotion',
+  yaka: '',
+  better: ' better.',
+  gameGoalParagraph1Part1: 'Alone or in a group,',
+  gameGoalParagraph1Part2:
+    'you are going to express your negative emotions with your voice and/or your gestures ',
+  gameGoalParagraph1Part3: 'following 3 essential steps to better tame them:',
+  gameGoalParagraph2Part1: "Haka's gestures coupled with the voice",
+  gameGoalParagraph2Part2: 'will enable you to find your hidden inner treasure',
+  gameGoalParagraph2Part3: 'that we all have : the',
+  powerParenthesis: '(power).',
+
+  //advice section
+  firstAdvice: 'Before each move, take a deep breath.',
+  secondAdvice: `If you don't have the time to play the "3 moves" mode, use the "1 move" mode. This version will enable you changing state quickly.`,
+  thirdAdvice: 'After your move(s) tell yourself:',
+  adviceFormula: '"The power is in me!"',
+  adviceFormulaExplanationPart1: 'This formula will reactivate your',
+  adviceFormulaExplanationPart2: '(power) at any moment!',
+
+  //gift cards
+  sendGiftCard: 'Send a gift card',
+  sendGiftCardTextPart1:
+    "You can send to someone (or yourself) one or several gift cards (Strength, Love, Confidence, Peace, Self-esteem, Calm, Energy, Joy) which will bring a positive energy to help one develop one's Mana on life's game field.",
+  sendGiftCardTextPart2:
+    'Feel free to choose the most appropriate card for you or the person you want to help. The helped person is free to send it to another person of her choice.',
+  chooseGiftCard: 'Choose a gift card',
+  sendThisGiftCard: 'Send this gift card',
+  giftCard: 'Gift card',
+  findManaWithGiftCard: 'Find your power (Mana) thanks to this gift card!',
+
+  //greeting cards
+  greetingText: 'Send a gift card to wish a Happy New year!',
+  sendThisGreetingCard: 'Send this greeting card',
+  greetingHappyNewYear: 'Happy New Year!',
+  greetingLetsPlay: "Let's play!",
+
+  //share app
+  shareSubject: "The Haka's Game Ludocoach",
+  shareMessage: `Find your power (Mana) thanks to the Haka's Game free app!`,
+
+  //authors
+  tehotuDescription: 'Tahitian, health coach and dancer',
+  hinenaoDescription: 'Marquesan, illustrator and health coach',
+  marcDescription: 'Parisian, games author and ludocoaching founder',
+};
---
+++ i18n/zh.js
@@ -0,0 +1,112 @@
+translations['zh'] = {
+    loading: '加载', 
+
+    //Jeu 游戏
+    hakaGame: '哈卡舞游戏',
+    guidedByLudocoach: '由远程教练Tehotu领舞',
+    play3Moves: '练习三个动作',
+    play1Move: '练习一个动作',
+    accessTraining: '训练通道',
+    allRightsReserved: '版权所有',
+    powerIsInYou: '力量与你同在！',
+
+    chooseCards: "根据心情在每种颜色中选择一张卡片，然后按顺序点击相应的卡片并跟随卡片的引导。",
+    oneMoveDescription: '使用这个版本，直接选择一张可以让你得到改善的卡牌。',
+    having: '现状', 
+    doing: '操练',
+    being: '成为',
+    play: '游戏',
+    next: '继续',
+    youHaveChosen: '您选择了',
+    placeYourself: "选择一个你感觉舒适并足够大的地方进行训练，深呼吸，然后跟我做。",
+    iChooseThisCard: '我选这张卡',
+    chooseAnotherCard: '选择另外一张卡',
+    manaActivated: '太棒了，您已经开启您的超自然力，请大声说：力量与我同在！',
+    thankYou: '谢谢',
+
+    //Menus 菜单
+    backToGame: '返回游戏',
+    gameGoalTitle: '游戏目的',
+    adviceMenuTitle: '建议',
+    sendGiftCardLabel: '发送一张礼物卡',
+    sendGreetingCardLabel: '发送一张心愿卡',
+    shareAppLabel: "分享软件",
+    contactUs: '联系我们',
+    authorsTitle: '其它',
+    gamesListTitle: 'Marc Kucharz的游戏', 
+    acknowledgementsMenu: '致谢',
+    acknowledgementsText: '致谢',
+    onFacebook: '在Facebook上', 
+    developedBy: '由……发起', 
+
+    //Cartes 卡片
+    abandonment: '放弃',
+    anger: '愤怒',
+    disgust: '厌倦',
+    doubt: '怀疑',
+    threat: '威胁',
+    fear: '害怕',
+    sadness: '悲伤',
+    violence: '暴力',
+    'joker-red': '红色王牌',
+    action: '奋斗',
+    courage: '勇气',
+    movement: '行动',
+    patience: '耐心',
+    preparation: '准备',
+    prevention: '预防',
+    protection: '保护',
+    unity: '统一',
+    'joker-orange': '橙色王牌',
+    love: '爱',
+    calm: '平静',
+    confidence: '信任',
+    energy: '能量',
+    'self-esteem': '自尊',
+    strength: '力量',
+    joy: '喜悦',
+    peace: '安宁',
+    'joker-green': '绿色王牌',
+
+    //game's goal section
+    transformYourEnergy:
+    '用毛利人祖先的舞蹈力量将您的消极能量转化为积极能量',
+    limitingEmotion: '可控情绪',
+    yaka: ' « Yaka »',
+    better: ' 更好',
+    gameGoalParagraph1: '独自一人或者几个人一起，你们将用声音和/或者动作的方式表达出你们的消极情绪,为了更好地转化这些情绪，请跟随三个重要步骤',
+    gameGoalParagraph2: '在haka的动作中加入呼喊，你们会自己找到我们每个人都拥有的极其珍贵的宝藏：力量',
+    powerParenthesis: ' (pouvoir).',
+
+    //advice section
+    firstAdvice: '每个动作之前，请做深呼吸。',
+    secondAdvice: `如果您没有时间完成“三个动作”，那就使用“一个动作”。这个版本可以使你很快地进入状态。`,
+    thirdAdvice: '在每次体验完哈卡舞之后，请大声说：',
+    adviceFormula: '"力量与我同在！"',
+    adviceFormulaExplanationPart1: '这种形式可以让你重新恢复活力！',
+    adviceFormulaExplanationPart2: '（力量）存在于任何时刻！',
+
+    //gift cards
+    sendGiftCard: '发送一张礼物卡',
+    sendGiftCardTextPart1:
+    "您可以给某人（或您自己）发送一张或者几张带给他（她）一种积极能量（力量、爱、信任、安宁、自尊、平静、能量、喜悦）的礼物卡，可以帮助他（她）在生命运动的大地上发展自己的超自然力。", 
+    sendGiftCardTextPart2:
+    '自由地选择一张最合适的卡送给您自己或您想要帮助的那个人；那个人也能自由地将此卡发送给他（她）选择要帮助的人。',
+    chooseGiftCard: '选择一张礼物卡',
+    sendThisGiftCard: '发送这张礼物卡',
+    giftCard: '礼物卡',
+    findManaWithGiftCard:
+    '由这张礼物卡而找到您的神力。',
+    //greeting cards
+    greetingText: '发送一张祝福新年的礼物卡！',
+    sendThisGreetingCard: '发送一张心愿卡。',
+    greetingHappyNewYear: '新年好！',
+    greetingLetsPlay: '轮到你了!',
+    //share app
+    shareSubject: '远程教练哈卡舞游戏',
+    shareMessage: `由这个免费的哈卡舞游戏找到您的神力！`,
+    //authors
+    tehotuDescription: '大溪地人，健身及舞蹈教练。',
+    hinenaoDescription: '马克萨斯群岛人，插图画家及健身教练。',
+    marcDescription: '巴黎人，游戏设计及远程教练发起人。',
+};
