14c0359979bba3310c5833a65dd81288a17a0318
Florent Roques <florentroques@users.noreply.github.com>
2024-11-24 21:55:21 +0100

    init

---
+++ i18n/fr.js
@@ -0,0 +1,127 @@
+translations['fr'] = {
+    loading: 'chargement',
+  
+    //Jeu
+    hakaGame: 'Le Jeu du Haka',
+    guidedByLudocoach: 'guidé par le ludocoach Tehotu',
+    play3Moves: 'Jouer en 3 mouvements',
+    play1Move: 'Jouer en 1 movement',
+    accessTraining: 'Accéder aux formations',
+    allRightsReserved: 'Tous droits réservés',
+    powerIsInYou: '"Le Pouvoir est en toi !"',
+  
+    chooseCards:
+      "Choisissez une carte de chaque couleur en fonction de vos émotions et laissez-vous guider en appuyant sur la carte correspondante dans l'ordre\u00a0: ",
+    oneMoveDescription:
+      'Pour cette version, choisissez la carte qui vous permettra de passer directement à un mieux-Être.',
+    having: 'AVOIR',
+    doing: 'FAIRE',
+    being: 'ÊTRE',
+    play: 'Jouer',
+    next: 'Continuer',
+    youHaveChosen: 'Vous avez choisi',
+    placeYourself:
+      ". Placez vous dans un endroit où vous vous sentez bien et avez assez d'espace. Respirez bien et faites comme moi.",
+    iChooseThisCard: 'Je choisis cette carte',
+    chooseAnotherCard: 'Choisir une autre carte',
+    manaActivated:
+      'Bravo, vous avez activé votre Mana ! Dites vous : "Le pouvoir est en moi !"',
+    thankYou: 'Merci',
+  
+    //Menus
+    backToGame: 'Retour au jeu',
+    gameGoalTitle: 'But du jeu',
+    adviceMenuTitle: 'Conseils',
+    sendGiftCardLabel: 'Envoyer une carte cadeau',
+    sendGreetingCardLabel: 'Envoyer une carte de voeux',
+    shareAppLabel: "Partager l'appli",
+    contactUs: 'Nous contacter',
+    authorsTitle: 'Auteurs',
+    gamesListTitle: 'Jeux de Marc Kucharz',
+    acknowledgementsMenu: 'Remerciements',
+    acknowledgementsText: 'Remerciements à ',
+    onFacebook: 'Sur Facebook',
+    developedBy: 'Développé par',
+  
+    //Cartes
+    abandonment: 'Abandon',
+    anger: 'Colère',
+    disgust: 'Dégoût',
+    doubt: 'Doute',
+    threat: 'Menace',
+    fear: 'Peur',
+    sadness: 'Tristesse',
+    violence: 'Violence',
+    'joker-red': 'Joker',
+    action: 'Action',
+    courage: 'Courage',
+    movement: 'Mouvement',
+    patience: 'Patience',
+    preparation: 'Préparation',
+    prevention: 'Prévention',
+    protection: 'Protection',
+    unity: 'Unité',
+    'joker-orange': 'Joker',
+    love: 'Amour',
+    calm: 'Calme',
+    confidence: 'Confiance',
+    energy: 'Energie',
+    'self-esteem': 'Estime de soi',
+    strength: 'Force',
+    joy: 'Joie',
+    peace: 'Paix',
+    'joker-green': 'Joker',
+  
+    //game's goal section
+    transformYourEnergy:
+      'Transformez votre énergie négative en énergie positive grâce au pouvoir des danses ancestrales maori !',
+    limitingEmotion: ' une émotion limitante',
+    yaka: ' « Yaka »',
+    better: ' mieux.',
+    gameGoalParagraph1Part1: 'Seul ou à plusieurs, vous allez exprimer',
+    gameGoalParagraph1Part2:
+      'par la parole et/ou les gestes vos émotions négatives',
+    gameGoalParagraph1Part3:
+      'pour mieux les dompter, en suivant 3 étapes essentielles :',
+    gameGoalParagraph2Part1: 'Les gestes du haka joints à la parole',
+    gameGoalParagraph2Part2:
+      'vont vous permettre de (re)trouver le trésor inestimable',
+    gameGoalParagraph2Part3: 'que nous possédons tous : le',
+    powerParenthesis: ' (pouvoir).',
+  
+    //advice section
+    firstAdvice: 'Avant chaque mouvement, respirez bien.',
+    secondAdvice: `Si vous n'avez pas le temps de jouer au mode "3 mouvements", utilisez le mode "1 mouvement". Cette version vous permettra de changer d'état rapidement.`,
+    thirdAdvice: 'Après avoir expérimenté le Jeu du Haka, dites-vous :',
+    adviceFormula: '"Le pouvoir est en moi !"',
+    adviceFormulaExplanationPart1: 'Cette formule vous permettra de réactiver',
+    adviceFormulaExplanationPart2: '(pouvoir) à tout moment !',
+  
+    //gift cards
+    sendGiftCard: 'Envoyer une carte cadeau',
+    sendGiftCardTextPart1:
+      "Vous pouvez envoyer à quelqu'un (ou à vous-même) une ou plusieurs cartes cadeaux (Force, Amour, Confiance, Paix, Estime de soi, Calme, Énergie, Joie) qui lui apportera une énergie positive pour l'aider à développer son Mana sur le terrain de jeu de la vie.",
+    sendGiftCardTextPart2:
+      'Libre à vous de choisir la carte la plus appropriée pour vous et/ou la personne que vous souhaitez aider. Libre à elle de l’envoyer à son tour à la personne de son choix.',
+    chooseGiftCard: 'Choisir une carte cadeau',
+    sendThisGiftCard: 'Envoyer cette carte cadeau',
+    giftCard: 'Carte cadeau',
+    findManaWithGiftCard:
+      'Retrouve ton pouvoir (Mana) grâce à cette carte cadeau !',
+  
+    //greeting cards
+    greetingText: 'Envoyez une carte cadeau pour souhaiter une bonne année !',
+    sendThisGreetingCard: 'Envoyer cette carte de voeux',
+    greetingHappyNewYear: 'Bonne année !',
+    greetingLetsPlay: 'A vous de jouer !',
+  
+    //share app
+    shareSubject: 'Le Jeu du Haka Ludocoach',
+    shareMessage: `Retrouve ton pouvoir (Mana) grâce à l'appli gratuite du Jeu du Haka !`,
+  
+    //authors
+    tehotuDescription: 'Tahitien, coach en bien-être et danseur',
+    hinenaoDescription: 'Marquisienne, illustratrice et coach en bien-être',
+    marcDescription: 'Parisien, auteur de jeux et fondateur du ludocoaching',
+  };
+  
